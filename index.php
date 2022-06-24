e48d090414f0aaf5128d6e8e38c56420649d9923
Besart Elezi <besart.elezi@hotmail.com>
2022-06-24 09:14:41 +0200

    update index, added buttons to flip through different exercises

--- index.php
+++ index.php
@@ -9,11 +9,34 @@ if(isset($_GET['page'])){
         case 'extending' :
             require 'exercise_2_extending.php';
             break;
+        case 'private' :
+            require 'exercise_3_private.php';
+            break;
+        case 'protected' :
+            require 'exercise_4_protected.php';
+            break;
+        case 'public' :
+            require 'exercise_5_public.php';
+            break;
+        case 'const' :
+            require 'exercise_6_const.php';
+            break;
+        case 'static' :
+            require 'exercise_7_static.php';
+            break;
         default :
             echo 'Hmmm whatcha say, Hmmm that you only meant to see a page? Well of course you did.';
     }
 }
 //create a form, method = GET, when user selects one of the options they go to the selected page
+//create buttons with value = page name that has been set in PHP code above
 ?>
- <button method ="get" formmethod="get" name ="classes">Classes</button>
-
+<form action ="index.php" method="get">
+    <button type="submit" name ="page" value ="classes">Classes</button>
+    <button type="submit" name ="page" value ="extending">extending</button>
+    <button type="submit" name ="page" value ="private">private</button>
+    <button type="submit" name ="page" value ="protected">protected</button>
+    <button type="submit" name ="page" value ="public">public</button>
+    <button type="submit" name ="page" value ="const">const</button>
+    <button type="submit" name ="page" value ="static">static</button>
+</form>
